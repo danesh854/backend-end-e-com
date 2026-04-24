a1909922da78bfcdd098abdce86cf2eb6b520611
danesh854 <itskabadedanesh@gmail.com>
2026-04-24 19:48:57 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/products', (req, res) => {
+    res.json([
+        { id: 1, name: "Laptop", price: 50000 },
+        { id: 2, name: "Phone", price: 20000 }
+    ]);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
