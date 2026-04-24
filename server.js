f90cf070235f5314d585dd337a83204b26296585
danesh854 <itskabadedanesh@gmail.com>
2026-04-24 20:00:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/products', (req, res) => {
     res.json([
         { id: 1, name: "Laptop", price: 50000 },
         { id: 2, name: "Phone", price: 20000 }
     ]);
+});
+
+app.get('/', (req, res) => {
+    res.send("Backend running");
 });
 
 app.listen(3000, () => {
